a9fd47c221b8fe734e16fe7b8118957dea0e8e9a
ElectroRaven <78317164+ElectroRaven@users.noreply.github.com>
2025-03-07 10:27:45 +0100

    Start and Stop command added

--- src/main/java/org/electroraven/randomItemXP/RandomItemXP.java
+++ src/main/java/org/electroraven/randomItemXP/RandomItemXP.java
@@ -1,10 +1,6 @@
 package org.electroraven.randomItemXP;
 
-import org.bukkit.Bukkit;
-import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
-import org.bukkit.Particle;
-import org.bukkit.Sound;
+import org.bukkit.*;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -27,55 +23,88 @@ import java.util.Random;
 public final class RandomItemXP extends JavaPlugin implements Listener {
     private final Random random = new Random();
     private final NamespacedKey placedBlockKey = new NamespacedKey(this, "placed");
+    private boolean isLevelRandomizerActive = false;
 
     @Override
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(this, this);
         Objects.requireNonNull(getCommand("getRandomizer")).setExecutor(this);
+        Objects.requireNonNull(getCommand("startlevelrandomizer")).setExecutor(this);
+        Objects.requireNonNull(getCommand("stoplevelrandomizer")).setExecutor(this);
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (sender instanceof Player) {
-            Player player = (Player) sender;
-            ItemStack randomizerItem = createRandomizerItem();
-            player.getInventory().addItem(randomizerItem);
-            player.sendMessage("You received a Randomizer!");
-            return true;
+        if (command.getName().equalsIgnoreCase("getRandomizer")) {
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+                ItemStack randomizerItem = createRandomizerItem();
+                player.getInventory().addItem(randomizerItem);
+                player.sendMessage("You received a Randomizer!");
+                return true;
+            }
         }
+
+        if (command.getName().equalsIgnoreCase("startlevelrandomizer")) {
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+                isLevelRandomizerActive = true;
+                player.sendMessage("Level Randomizer is now active!");
+                return true;
+            }
+        }
+
+        if (command.getName().equalsIgnoreCase("stoplevelrandomizer")) {
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+                isLevelRandomizerActive = false;
+                player.sendMessage("Level Randomizer has been deactivated.");
+                return true;
+            }
+        }
+
         return false;
     }
 
     @EventHandler
     public void onPlayerUse(PlayerInteractEvent event) {
-        Player player = event.getPlayer();
-        if (player.getInventory().getItemInMainHand().getType() == Material.STICK) {
-            int levels = player.getLevel();
-            if (levels > 0) {
-                player.setLevel(0);
-                ItemStack randomItem = createRandomItem(levels);
-                player.getInventory().addItem(randomItem);
-                player.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1f, 1f);
-                player.spawnParticle(Particle.ENCHANT, player.getLocation(), 10);
+        if (isLevelRandomizerActive) {
+            Player player = event.getPlayer();
+            if (player.getInventory().getItemInMainHand().getType() == Material.STICK) {
+                int levels = player.getLevel();
+                if (levels > 0) {
+                    player.setLevel(0);
+                    ItemStack randomItem = createRandomItem(levels);
+                    player.getInventory().addItem(randomItem);
+                    player.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1f, 1f);
+                    player.spawnParticle(Particle.ENCHANT, player.getLocation(), 10);
+                }
             }
         }
     }
 
     @EventHandler
     public void onBlockPlace(BlockPlaceEvent event) {
-        event.getBlock().getState().setMetadata("placed", new org.bukkit.metadata.FixedMetadataValue(this, true));
+        if (isLevelRandomizerActive) {
+            // Markiere den Block als vom Spieler platziert
+            event.getBlock().getState().setMetadata("placed", new org.bukkit.metadata.FixedMetadataValue(this, true));
+        }
     }
 
     @EventHandler
     public void onBlockBreak(BlockBreakEvent event) {
-        if (!event.getBlock().hasMetadata("placed")) {
-            event.setDropItems(false); // Wenn der Block nicht platziert wurde, keine Drops
+        if (isLevelRandomizerActive) {
+            if (!event.getBlock().hasMetadata("placed")) {
+                event.setDropItems(false); // Wenn der Block nicht platziert wurde, keine Drops
+            }
         }
     }
 
     @EventHandler
     public void onMobDeath(EntityDeathEvent event) {
-        event.getDrops().clear();
+        if (isLevelRandomizerActive) {
+            event.getDrops().clear(); // Entfernen aller Mob-Drops
+        }
     }
 
     private ItemStack createRandomizerItem() {
@@ -93,16 +122,15 @@ public final class RandomItemXP extends JavaPlugin implements Listener {
         Material material = getRandomMaterial();
         ItemStack item = new ItemStack(material, amount);
         ItemMeta meta = item.getItemMeta();
+        if (meta != null) {
+            item.setItemMeta(meta);
+        }
         return item;
     }
 
     private Material getRandomMaterial() {
         List<Material> materials = List.of(Material.values());
         return materials.get(random.nextInt(materials.size()));
-    }
-
-    private boolean isPlayerPlaced(Material material) {
-        return material.isBlock();
     }
 
     @Override
